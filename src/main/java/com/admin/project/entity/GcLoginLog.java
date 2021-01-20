def0bfa41ab17d2f6031d815cfdb5793885f76bc
dunn <dunn.work.gmail>
2021-01-20 20:10:35 +0800

    添加用户登录日志

---
+++ src/main/java/com/admin/project/entity/GcLoginLog.java
@@ -0,0 +1,46 @@
+package com.admin.project.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+/**
+ * 会员基本信息表
+ */
+@Data
+@TableName("gc_login_log")
+public class GcLoginLog {
+    @Id
+    @TableId(value = "id", type = IdType.AUTO)
+    @ApiModelProperty(value = "login_log_id")
+    private Integer loginLogId ;
+
+    @ApiModelProperty("用户名")
+    @TableField("user_name")
+    private String userName;
+
+    @ApiModelProperty("用户名")
+    @TableField("user_id")
+    private int userId;
+
+    @ApiModelProperty(value = "登录时间")
+    @TableField("create_time")
+    private Date createTime;
+
+    @ApiModelProperty(value = "登录状态")
+    private boolean succeed;
+
+    @ApiModelProperty(value = "异常信息")
+    private String mes;
+
+    @ApiModelProperty(value = "登录Ip")
+    @TableField("ip_address")
+    private String ipAddress;
+
+}
---
+++ src/main/java/com/admin/project/mapper/GcLoginLogMapper.java
@@ -0,0 +1,10 @@
+package com.admin.project.mapper;
+
+import com.admin.project.entity.GcLoginLog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 会员信息表
+ */
+public interface GcLoginLogMapper extends BaseMapper<GcLoginLog>{
+}
